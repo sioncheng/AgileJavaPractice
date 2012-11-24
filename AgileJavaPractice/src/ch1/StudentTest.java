39ae59e7f8e29901e1b08c7f759bdb2dbba411a1
sioncheng <chengsion@hotmail.com>
2012-11-24 15:24:24 +0800

    ch1

---
+++ AgileJavaPractice/src/ch1/StudentTest.java
@@ -0,0 +1,20 @@
+package ch1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		
+		Student student1 = new Student("Jon");
+		assertEquals("Jon",student1.getName());
+		
+		Student student2 = new Student("Joy");
+		assertEquals("Joy",student2.getName());
+	}
+
+}
